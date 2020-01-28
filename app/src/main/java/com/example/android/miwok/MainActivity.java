490b9580c057bb164381373ae79f25c9c4c7eb6c
martha <m.v.serpak@gmail.com>
2020-01-28 19:57:14 +0200

    00

--- app/src/main/java/com/example/android/miwok/MainActivity.java
+++ app/src/main/java/com/example/android/miwok/MainActivity.java
@@ -1,58 +1,33 @@
 package com.example.android.miwok;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+import com.google.android.material.tabs.TabLayout;
+
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView numbers = findViewById(R.id.numbers);
-        TextView colors = findViewById(R.id.colors);
-        TextView family = findViewById(R.id.family);
-        TextView phrases = findViewById(R.id.phrases);
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewPager);
+        CategoryAdapter adapter = new CategoryAdapter(this,getSupportFragmentManager());
+        viewPager.setAdapter(adapter);
 
-        numbers.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent numInt = new Intent(MainActivity.this,
-                        NumbersActivity.class);
-                startActivity(numInt);
-            }});
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.Tabs);
+        tabLayout.setupWithViewPager(viewPager);
 
-        colors.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent colInt = new Intent(MainActivity.this,
-                        ColorsActivity.class);
-                startActivity(colInt);
-            }
-        });
-
-        family.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent famInt = new Intent(MainActivity.this,
-                        FamilyActivity.class);
-                startActivity(famInt);
-            }
-        });
-
-        phrases.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent phrInt = new Intent(MainActivity.this,
-                        PhrasesActivity.class);
-                startActivity(phrInt);
-            }
-        });
     }
-
 }
